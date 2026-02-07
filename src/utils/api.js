7e77034a385909daaa07255b575ce31fe3877c18
Ravi Chaudhary <6312ravi6312@gmail.com>
2026-02-08 01:30:02 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 // Request interceptor to add auth token
@@ -36,4 +36,5 @@ export const endInning = (matchId) => {
 
 
 export default api;
+
 
