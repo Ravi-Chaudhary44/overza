f7c45c23b5421ed8b0fe840e644227db6ff5cdcc
Ravi Chaudhary <6312ravi6312@gmail.com>
2026-02-08 01:00:10 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-// Create axios instance with base URL
+
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
 });
@@ -29,10 +29,11 @@ api.interceptors.response.use(
   }
 );
 
-// API methods
+
 export const endInning = (matchId) => {
   return api.post(`/score/${matchId}/end-inning`);
 };
 
-// Add more API calls here as needed
+
 export default api;
+
